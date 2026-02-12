764e96f34b0eda11af4b9ad0a4082a2dff98448e
dharmpreetgithub <148900042+dharmpreetgithub@users.noreply.github.com>
2026-02-12 19:52:00 +0530

    Implement basic TOPSIS function in topsis_app.py
    Added a basic TOPSIS function to process decision-making data from a CSV file.

--- topsis_app.py
+++ topsis_app.py
@@ -2,6 +2,44 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+def basic_topsis(input_file, weights, impacts):
+
+    data = pd.read_csv(input_file)
+
+    decision_matrix = data.iloc[:, 1:]
+
+    # Normalize
+    norm = decision_matrix / np.sqrt((decision_matrix**2).sum())
+
+    # Convert weights
+    weights = np.array(weights)
+
+    weighted_matrix = norm * weights
+
+    ideal_best = []
+    ideal_worst = []
+
+    for i in range(len(impacts)):
+        if impacts[i] == '+':
+            ideal_best.append(weighted_matrix.iloc[:, i].max())
+            ideal_worst.append(weighted_matrix.iloc[:, i].min())
+        else:
+            ideal_best.append(weighted_matrix.iloc[:, i].min())
+            ideal_worst.append(weighted_matrix.iloc[:, i].max())
+
+    ideal_best = np.array(ideal_best)
+    ideal_worst = np.array(ideal_worst)
+
+    dist_best = np.sqrt(((weighted_matrix - ideal_best)**2).sum(axis=1))
+    dist_worst = np.sqrt(((weighted_matrix - ideal_worst)**2).sum(axis=1))
+
+    score = dist_worst / (dist_best + dist_worst)
+
+    data['Topsis Score'] = score
+    data['Rank'] = score.rank(ascending=False)
+
+    return data
+
 st.title("TOPSIS Decision Making Tool")
 
 st.write("Upload a CSV file and provide weights and impacts.")
